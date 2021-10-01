4f3f062396d26df4331c66e8442282fe2827435b
enigmaticrex <56771208+enigmaticrex@users.noreply.github.com>
2021-10-01 11:09:47 +0000

    Minor Changes

--- menus.c
+++ menus.c
@@ -31,9 +31,6 @@ void sub_heading(char *str,char *symbol,int number,int high)
     {
         
         int size = high+6;
-        printf(" ");
-        symbol_print(size,symbol);
-        printf("\n");
         printf("  %d. %s ",number,str);
         printf(" ");
         printf(" ");
@@ -49,7 +46,6 @@ int main(void)
     char hard_string[10][100] = {"Main Menu","Sub Menu One","Sub Menu Three","Sub Menu Four"};
     char symbols[3][3] = {"=","*"};
     
-    
     int i,j,high=0;
     for(i =1;i<=3;i++)
     {
@@ -59,6 +55,9 @@ int main(void)
       }
     }
     title_print(hard_string[0],symbols[1]);
+    printf(" ");
+    symbol_print(high+6, symbols[0]);
+    printf("\n");
     sub_heading(hard_string[1], symbols[0],1,high);
     sub_heading(hard_string[2], symbols[0],2,high);
     sub_heading(hard_string[3], symbols[0],3,high);

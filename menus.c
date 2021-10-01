d01d698375b04300969ba7d5ebfc2cd2ede92b8d
enigmaticrex <56771208+enigmaticrex@users.noreply.github.com>
2021-10-01 08:59:03 +0000

    Added Sub Menus. Bugs exists

--- menus.c
+++ menus.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 void symbol_print(int size,char *symbol)
         {
           int i;
@@ -26,13 +27,28 @@ void title_print(char *str,char *symbol)
         symbol_print(size,symbol);
         printf("\n");
     }
+void sub_heading(char *str,char *symbol)
+    {
+        
+        int size = strlen(str)+3;
+        printf("|");
+        printf("  1. %s ",str);
+        printf(" ");
+        printf("|");
+        printf(" ");
+        printf("\n");
+        printf(" ");
+        symbol_print(size,symbol);
+        printf("\n");
+    }
+
 int main(void)
 {
 
-    char hard_string[10] = "Menu";
-    char symbols[3][10] = {"=","*"};
+    char hard_string[10][10] = {"Main Menu","Sub Menu"};
+    char symbols[3][3] = {"=","*"};
     
-    title_print(hard_string,symbols[0]);
-    title_print(hard_string,symbols[1]);
+    title_print(hard_string[0],symbols[1]);
+    sub_heading(hard_string[1], symbols[0]);
     return 0;
 }
